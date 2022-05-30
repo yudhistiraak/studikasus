79317862333ed0407096332b7d9b6d3d7e9ba500
yudhistiraak <Yudhistira.Kurniawan@atlantica.dev>
2022-05-30 23:24:09 +0700

    update index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { initModels, product, order, productCreationAttributes, orderCreationAtt
 import * as dotenv from 'dotenv';
 import { ApolloServer, gql } from "apollo-server";
 import { readFileSync } from "fs";
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 const typeDefs = readFileSync("./src/product-order.graphql").toString('utf-8')
@@ -50,8 +51,33 @@ const resolvers = {
         },
         DeleteProduct: (_parent: any, args: any) => {
             return product.destroy({ where: { id: args.id } });
+        },
+        // ORDER
+        GetDetailOrder: async (_parent: any, args: any) => {
+            return await order.findByPk(args.id)
+        },
+        CreateOrder: async (_parent: any, args: any) => {
+            const now = new Date();
+            const generator = uuidv4();
+            
+            const newOrder: orderCreationAttributes = {
+                transcode:generator,
+                created: now.toDateString(),
+            }
+            return await order.create(newOrder)
+        },
+        UpdateOrder: async (_parent: any, args: any) => {
+            const generator = uuidv4();
+            const updOrder = {
+                transcode: generator,
+            };
+            await order.update(updOrder, { where: { id: args.id } });
+            return await order.findByPk(args.id);
+        },
+        DeleteOrder: (_parent: any, args: any) => {
+            order.destroy({ where: { id: args.id } });
+            return order.findByPk(args.id);
         }
-
     }
 }
 
